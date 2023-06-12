01b1f3c75be1a9c08d1f03d81d0c342e75641185
kubas33 <Stasiak91@gmail.com>
2023-06-12 13:52:33 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const input = document.querySelector("input");
+const mailError = document.getElementById("mail-error");
+const form = document.querySelector("form");
+
+input.addEventListener("blur", handleInput);
+
+function handleInput() {
+    if (input.validity.valueMissing) {
+        mailError.textContent = "Email cannot be empty";
+        input.classList.add("invalid");
+        console.log("Email cannot be empty");
+    } else if (input.validity.typeMismatch) {
+        mailError.textContent = "Please provide a valid email address";
+        input.classList.add("invalid");
+        console.log("Please provide a valid email address");
+    } else {
+        mailError.textContent("");
+        input.classList.remove("invalid");
+    }
+}
+
+form.addEventListener("submit", event => {
+    event.preventDefault();
+    handleInput.call();
+})
