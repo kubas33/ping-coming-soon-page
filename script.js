dc25fbb3db5a273ec185cc6a6b92128ddb1816b3
kubas33 <Stasiak91@gmail.com>
2023-06-19 07:49:04 +0200

    Improve styling due to changes in HTML

--- script.js
+++ script.js
@@ -7,15 +7,18 @@ input.addEventListener("blur", handleInput);
 function handleInput() {
     if (input.validity.valueMissing) {
         mailError.textContent = "Whoops! It looks like you forgot to add your email";
+        mailError.classList.add("visible-error");
         input.classList.add("invalid");
         console.log("Email cannot be empty");
     } else if (input.validity.typeMismatch) {
         mailError.textContent = "Please provide a valid email address";
         input.classList.add("invalid");
+        mailError.classList.add("visible-error");
         console.log("Please provide a valid email address");
     } else {
-        mailError.textContent("");
+        mailError.textContent = "";
         input.classList.remove("invalid");
+        mailError.classList.remove("visible-error");
     }
 }
 
